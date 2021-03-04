dc75f2e445823c56eb024b4d022b556c270b9e11
Samuel Lamontagne <samuel.lamontagne.86@gmail.com>
2021-03-04 15:22:51 -0500

    First commit

---
+++ src/stockMarketSim.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : stockMarketSim.cpp
+// Author      : Samuel Lamontagne
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
