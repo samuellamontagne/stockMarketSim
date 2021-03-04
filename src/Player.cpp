2c1cc5b0ead83ce7a4930c10a685106cbb7cb3de
Qin <qin.shao.ca@gmail.com>
2021-03-05 07:40:29 +0800

    add calculatProfit() add asset

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,18 +4,19 @@
 
 #include "Player.h"
 
-int Player::getTotalMoney() const {
+
+double Player::getCash() const {
     return cash;
 }
 
-void Player::setTotalMoney(int totalMoney) {
-    Player::cash = totalMoney;
+void Player::setCash(double cash) {
+    Player::cash = cash;
 }
 
 
-
 void Player::buy(Instruments* instrument, double price, int quantity) {
-    if(price*quantity < cash) {
+    if(price*quantity <= cash) {
+        cash-=price*quantity;
         // on market side
         instrument->setQuantity(instrument->getQuantity()-quantity);
         // on player side
@@ -39,14 +40,20 @@ void Player::buy(Instruments* instrument, double price, int quantity) {
 
 
 void Player::sell(Instruments &instrument, double price, int quantity) {
-    for (auto & i : playerAccount) {
-        if(i->getName() == instrument.getName() ){
-            // if I have enough quantity I could sell
-            if(i->getQuantity() >= quantity){
+    for (int i =0 ;i < playerAccount.size();i++) {
+        if(playerAccount.at(i)->getName() == instrument.getName() ){
+            // if player have enough quantity player could sell
+            if(playerAccount.at(i)->getQuantity() > quantity){  // still have this object
                 instrument.setQuantity(instrument.getQuantity()+quantity);
-                i->setQuantity(i->getQuantity()-quantity);
+                playerAccount.at(i)->setQuantity(playerAccount.at(i)->getQuantity()-quantity);
+                cash+=price*quantity;
                 break;
-            } else {
+            } else if(playerAccount.at(i)->getQuantity() == quantity){ // not have this stock
+                playerAccount.erase(playerAccount.begin()+i);  // remove from playerAccount
+                cash+=price*quantity;
+                break;
+            }
+            else {
                 cout << " not enough quantity";
             }
         } else{
@@ -62,6 +69,36 @@ const vector<Instruments *> &Player::getPlayerAccount() const {
 void Player::setPlayerAccount(const vector<Instruments *> &list) {
     Player::playerAccount = list;
 }
+
+double Player::calculateAsset(const vector<struct Instruments *> list) const {
+    if(playerAccount.empty()){
+        return cash;
+    }else{
+        double subtotal=0;
+        for (auto i : playerAccount) {
+            for (auto j : list) {
+                if(i->getName()==j->getName()){
+                    subtotal+=i->getQuantity()*j->getPrice();
+                    break;
+                }
+            }
+
+        }
+        return cash+subtotal;
+    }
+}
+
+
+
+double Player::getProfile(const vector<struct Instruments *> list) const {
+    if(playerAccount.empty()){
+       return cash-STARTING_CASH;
+    }else{
+      return  calculateAsset(list)-STARTING_CASH;
+    }
+
+}
+
 
 
 
--- src/Player.h
+++ src/Player.h
@@ -7,23 +7,32 @@
 #include "Instruments.h"
 #include "Bond.h"
 #include <vector>
+#define STARTING_CASH 10000;
 
 class Player {
+
+
 private:
-    int cash=100; // start money
+    double cash=STARTING_CASH; // start money
+    double asset=STARTING_CASH;
     vector<Instruments*> playerAccount;  // hold vary  bond stock
 public:
-    int getTotalMoney() const;
-
     const vector<Instruments *> &getPlayerAccount() const;
-
     void setPlayerAccount(const vector<Instruments *> &playerAccount);
+    double getProfile(vector<struct Instruments *> list) const;
 
-    void setTotalMoney(int totalMoney);
 
     void  sell(Instruments &instrument, double price, int quantity);
 
     void buy(Instruments *instrument, double price, int quantity);
+
+    double getCash() const;
+
+    void setCash(double cash);
+
+    static double getCurrentPrice(Instruments *instrument);
+
+    double calculateAsset(vector<struct Instruments *> list) const;
 };
 
 
